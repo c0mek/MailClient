4151eb7b5dd83d3c6105318637613b019fe07ce7
c0mek <84935945+c0mek@users.noreply.github.com>
2021-10-01 11:19:06 +0200

    siema
    simea

---
+++ program/program/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassMail;
+
+namespace program
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            Mail m = new Mail();
+            m.attachment = "siema";
+            Console.WriteLine(m.attachment);
+            InitializeComponent();
+        }
+    }
+}
